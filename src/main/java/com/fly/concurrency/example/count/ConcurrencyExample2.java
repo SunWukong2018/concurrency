db0a8d65ef1f3bc23bbf6d9731006607589b8bd2
chengwei <wei-cheng@neusoft.com>
2018-04-20 18:05:56 +0800

    删除多导入的包

--- src/main/java/com/fly/concurrency/example/count/ConcurrencyExample2.java
+++ src/main/java/com/fly/concurrency/example/count/ConcurrencyExample2.java
@@ -4,7 +4,6 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.LongAdder;
 
 import com.fly.concurrency.annoations.ThreadSafe;
