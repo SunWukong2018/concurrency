f3b80989b0447d8f5271b4ad6672677ee64aaa95
chengwei <wei-cheng@neusoft.com>
2018-04-25 17:25:21 +0800

    线程安全容器

---
+++ src/main/java/com/fly/concurrency/example/concurrent/CopyOnWriteArraySetExample.java
@@ -0,0 +1,50 @@
+package com.fly.concurrency.example.concurrent;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+import com.fly.concurrency.annoations.ThreadSafe;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ThreadSafe
+public class CopyOnWriteArraySetExample {
+
+    private static int          clientTotal = 5000;                       // 请求总数
+
+    private static int          threadTotal = 200;                        // 同时并发执行线程数
+
+    private static Set<Integer> list        = new CopyOnWriteArraySet<>();
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);// 定义信号量
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);// 计数器值设为5000
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            exec.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add(count);
+                    semaphore.release();// 释放信号量
+                } catch (Exception e) {
+                    log.error("exception:{}", e);
+                }
+                countDownLatch.countDown();// 计数器值运行一次减1
+            });
+        }
+        countDownLatch.await();// 保证计数器值减为0
+        exec.shutdown();// 关闭线程池
+        log.info("size:{}", list.size());
+    }
+
+    static void add(final int i) {
+        list.add(i);
+    }
+
+}
